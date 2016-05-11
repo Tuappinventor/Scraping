768b99540097a3f8eaa2756ffa5486f17fe0b695
Jaime <jaime.chapinal@gmail.com>
2016-05-12 00:24:40 +0200

    Overwritting images and avoid exceptions

--- injuriesLeagueBBVA/scrapInjuries.py
+++ injuriesLeagueBBVA/scrapInjuries.py
@@ -1,65 +1,159 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
 
-class PlayerInjury:
-  name = ''
-  injury = ''
-  hash = 0
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
-def findInjuriesElement(injury):
-    parent = injury.find_element_by_xpath('..')
-    parent = parent.find_element_by_xpath('..') #<span style="color: #000000;"><span style="text-decoration: underline;">LESIONADOS</span><br></span>
-    parent = parent.find_element_by_xpath('..') #<strong>
-    parent = parent.find_element_by_xpath('..') #<td style="text-align: center; background-color: #f5f5f5; width: 100%;"
-    return parent.find_element_by_xpath("following-sibling::*");
+class Match:
+  date = ''
+  competition = ''
+  teams = ''
+  local = True
+  link = ''
 
-def writeFileResults(nameFile, listPlayers):
+
+def isLocalTeam(teams):
+    defaultTeam = u'Atl\xe9tico'
+    teamsArr = teams.split("-")
+    return True if (teamsArr[0] == defaultTeam) else False
+
+def getMatches():
+    driver.implicitly_wait(5)
+    driver.set_page_load_timeout(11)
+    
+    url = "http://www.marca.com/deporte/futbol/equipos/atletico/resultados-temporada.html"
+    try:
+        driver.get(url)
+    except:
+        print "Time out because of ads... but trying to go ahead!"
+
+    table_id = driver.find_element_by_id('resultadosCompletos');
+    rows = table_id.find_elements_by_tag_name("tr") # get all of the rows in the table
+    for row in rows:
+        # Get the columns (all the column 2)
+        if row.get_attribute("class") == "encabezado":
+            continue
+
+        cols = row.find_elements_by_tag_name("td")
+
+        match = Match()
+        match.date = cols[0].text
+        match.competition = cols[1].text
+        match.teams = cols[2].text
+        match.local = isLocalTeam(match.teams)
+        match.link = cols[3].find_element_by_tag_name("a").get_attribute("href")
+
+        listMatches.append(match)
+
+    driver.quit()
+
+def addOverlayImage(file):
+    textFile = os.path.basename(file)
+    text = textFile[textFile.index('_') + 1:]
+    text = text.replace(".png", "")
+
+    img = Image.open(file)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("TektonPro-BoldExt.otf", 18)
+    draw.text((70, 0), text, (0,0,0), font=font)
+    img.save(textFile)
+
+def saveScreenshot(element, namePNG, driver):
+    location = element.location
+    size = element.size
+    driver.save_screenshot(namePNG) # saves screenshot of entire page
+    driver.quit()
+
+    im = Image.open(namePNG) # uses PIL library to open image in memory
+
+    left = location['x']
+    top = location['y']
+    right = location['x'] + size['width']
+    bottom = location['y'] + size['height']
+
+    im = im.crop((left, top, right, bottom)) # defines crop points
+    im.save(namePNG) # saves new cropped image
+
+    addOverlayImage(namePNG)
+
+def getMatchInfo(match, current_match, raw):
+    time.sleep(2)
+
+    driver = webdriver.Firefox()
+    driver.set_window_position(0,0)
+    driver.delete_all_cookies()
+
+    #url = "http://www.marca.com/eventos/marcador/futbol/2015_16/champions/semifinal/ida/atm_bay/"
+    driver.get(match.link)
+
+    time.sleep(10)
+
+    try:
+        if driver.find_element_by_xpath("//*[contains(text(), 'O. INICIALES')]"):
+            oncesIniciales = driver.find_elements_by_xpath("//*[contains(text(), 'O. INICIALES')]");
+
+            if len(oncesIniciales) > 0:
+                oncesIniciales[0].click()
+
+                raw += match.teams + ","
+
+                # Local team = lTeamAlign
+                teamElement = "lTeamAlign" if (match.local) else "vTeamAlign"
+                atletiElem = driver.find_elements_by_class_name("green")[0].find_element_by_class_name("iniciales").find_element_by_class_name(teamElement)
+
+                alignmentText = None
+                alineacionElem = "alignment" if (match.local) else "vAlignment"
+                if atletiElem.find_element_by_class_name(alineacionElem):
+                    alignment = atletiElem.find_element_by_class_name(alineacionElem).text
+                    raw += alignment + ","
+
+                lines = atletiElem.find_elements_by_class_name("plLine");
+                for line in lines:
+                    players = line.find_elements_by_class_name("player");
+                    for player in players:
+                        name = player.find_element_by_class_name("pl-name").text.encode("utf-8");
+                        raw += name.encode('utf-8') + "\t"
+
+                raw += "\n"
+                saveScreenshot(atletiElem, "Atleti/" + str(current_match) + "_" + match.teams + ".png", driver);
+
+    except:
+        print "Element not found. Continue..."
+        driver.quit()
+
+
+def writeFileResults(nameFile, raw):
     with open(nameFile, 'w+') as fResults:
-        out = "Player,Injury"
-        fResults.write(out.encode('utf8') + '\n')
-        for pl in listPlayers:
-            out = pl.name + "," + pl.injury
-            fResults.write(out.encode('utf8') + '\n')
+        fResults.write(raw.encode('utf8'))
+
 
 if __name__ == "__main__":
+
     driver = webdriver.Firefox()
     driver.maximize_window()
     driver.delete_all_cookies()
 
-    url = "http://www.goal.com/es/news/27/liga-de-espa%C3%B1a/2015/12/30/4730098/sancionados-lesionados-y-apercibidos"
-    driver.get(url)
 
-    # List all players that we're storing
-    listPlayers = []
+    raw = ""
 
-    injuries = driver.find_elements_by_xpath("//*[contains(text(), 'LESIONADOS')]")
-    for injury in injuries:
-        # Finding the correct elements to scrap.
-        injuElem = findInjuriesElement(injury)
-        children = injuElem.find_elements_by_xpath('./*')
-        if len(children) < 1:
-            continue
+    listMatches = []
+    getMatches()
+    i = 1
+    for match in listMatches:
+        current_match = len(listMatches) - i
+        getMatchInfo(match, current_match, raw)
+        i += 1
 
-        numberOfPlayers = len(children[0].text.split('\n'))
-        instancelist = [PlayerInjury() for i in range(numberOfPlayers)]
-
-        counterText = 0
-        for child in children[:-1]:
-            counterList = 0
-            listText = child.text.split('\n')
-            for text in listText:
-                if counterText == 0:
-                    instancelist[counterList].name = text.strip()
-                else:
-                    instancelist[counterList].injury = text.strip()
-                counterList += 1
-            counterText += 1
-
-        for player in instancelist:
-            player.hash = hash(player.name + player.injury)
-            if player.hash not in listPlayers:
-                listPlayers.append(player)
-
-    nameFile = "injuriesJornada31.csv"
-    writeFileResults(nameFile, listPlayers)
+    writeFileResults('atleti.csv', raw)
 
     driver.quit()
+
+    print "- end -"
+
+
