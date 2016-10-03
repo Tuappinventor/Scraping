24fdac2fd43cb2f8138b731a95ac04825f0c59d1
Xatpy <jaime.chapinal@gmail.com>
2016-10-04 00:38:49 +0200

    Adding functionality to detect the three most similar players in Atletico de Madrid

--- FootballPlayerFaces/faceApiDetect.py
+++ FootballPlayerFaces/faceApiDetect.py
@@ -4,14 +4,29 @@
 import csv
 import cognitive_face as CF
 
-# Create new list
+class PlayerInfo(object):
+    name = ""
+    photoUrl = ""
+    faceId = ""
+
+def loadPlayersInfo(playersInfoList, file):
+    with open(file, 'rb') as csvReadFile:
+        csvReader = csv.reader(csvReadFile, delimiter=',', quotechar='|')
+        for row in csvReader:
+            player = PlayerInfo()
+            player.name = row[1]
+            player.photoUrl = row[2]
+            player.faceId = row[3]
+            playersInfoList.append(player)
+
+def getPlayerNameByFaceId(playersInfoList, faceId):
+    for playerInfo in playersInfoList:
+        if playerInfo.faceId == faceId:
+            return playerInfo
+    return None
+
 def createNewFacesList(idList, nameList):
     CF.face_list.create(idList, nameList)
-
-# ---MAIN---
-
-KEY = "YOUR_KEY"
-CF.Key.set(KEY)
 
 def createFaceList():
     idList = 2
@@ -23,15 +38,37 @@ def createFaceList():
              for row in csvReader:
                  # Add face to the list
                  ret = CF.face_list.add_face(row[2], idList)
+                 print "Face " + str(ret[u'persistedFaceId']) + " added to list " + str(idList)
                  csvWriter.writerow([row[0], row[1], row[2], ret[u'persistedFaceId']])
 
-def getComparationFaces(urlFace):
+def getComparationFaces(urlFace, faceListId, playersInfoList):
     ret = CF.face.detect(urlFace)
-    ret = CF.face.find_similars(ret[0][u'faceId'], 1, None, 1000, 'matchFace')
+    ret = CF.face.find_similars(ret[0][u'faceId'], faceListId, None, 1000, 'matchFace')
     if (len(ret) == 0):
         print "No faces in the list"
         return
 
-    print str(ret[0][u'persistedFaceId']) + "----" + str(ret[0][u'confidence'])
+    cont = 0
+    for cont in range(3):
+        faceId = str(ret[cont][u'persistedFaceId'])
+        confidence = str(ret[cont][u'confidence'])
+        player = getPlayerNameByFaceId(playersInfoList, faceId)
+        if player == None:
+            return
+        print "#" + str((cont + 1)) + ") " + str(player.name) + " --- " + confidence + " --- " + player.photoUrl
 
-getComparationFaces("http://www.fichajes.net/files/simeone_1_1.jpeg")
+# ---MAIN---
+
+KEY = 'YOUR_KEY'
+CF.Key.set(KEY)
+
+idList = 2
+#createFaceList(idList)
+
+file = "atletiCarasOut.csv"
+playersInfoList = []
+loadPlayersInfo(playersInfoList, file)
+
+urlFace = "http://img.estaticos-atleticodemadrid.com/system/fotos/603/showlarge/moya_web.jpg?1413539984"
+getComparationFaces(urlFace, idList, playersInfoList)
+
